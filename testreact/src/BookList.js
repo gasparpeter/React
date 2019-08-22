834aae9eb127a9f0dd6f4bf40bd062481bb2c308
Peter Gaspar <petihappygaspar@gmail.com>
2019-08-22 22:10:19 +0300

    - classes - added booklist.js , the functions is works

---
+++ testreact/src/BookList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BookList extends Component{
+
+    constructor() {
+        super();
+        console.log(1)
+    }
+
+    render() {
+        return (
+            <div>Book List</div>
+    );
+    }
+
+}
+
+export default BookList;
