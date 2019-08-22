94482d375ec0eefbd1b98ceb8e8c4ceb2be2206d
Peter Gaspar <petihappygaspar@gmail.com>
2019-08-22 21:30:38 +0300

    - booktitle

--- testreact/src/Book.js
+++ testreact/src/Book.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Book = (props) => {
+const BookTitle = (props) => {
   return <div>{props.title}</div>
+};
+
+const Book = (props) => {
+  return (<div>
+    <BookTitle title = {props.title}/>
+    </div>
+)
 };
 
 export default Book;
